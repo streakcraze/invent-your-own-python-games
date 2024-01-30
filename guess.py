f0638585f234d53c82765cb88a64cecf625e2b2f
Arnold Obiero <odhisoarnold@gmail.com>
2024-01-31 02:16:28 +0300

    bound variables

--- guess.py
+++ guess.py
@@ -1,8 +1,7 @@
 """This is a Guess the Number game."""
 
+import re
 import random
-
-guesses_taken = 0
 
 print("Hello! What is your name?")
 username = input()
@@ -11,9 +10,11 @@ secret_number = random.randint(1, 20)
 
 print(f"Well, {username}, I am thinking of a number between 1 and 20.")
 
-for guesses_taken in range(6):
-    print("Take a guess.")
-    user_guess = input()
+for guesses_taken in range(1, 7):
+    user_guess = ""
+    while re.match("^[1-9][0-9]*$", user_guess) is None or int(user_guess) > 20:
+        print("Take a guess. (1~20)")
+        user_guess = input()
     user_guess = int(user_guess)
 
     if user_guess > secret_number:
@@ -21,12 +22,11 @@ for guesses_taken in range(6):
     elif user_guess < secret_number:
         print("Your guess is too low.")
     else:
+        print(
+            f"Good job, {username}! You guessed my number in {guesses_taken} {'try' if guesses_taken == 1 else 'tries'}"
+        )
         break
-
-if user_guess == secret_number:
-    guesses_taken += 1
-    print(
-        f"Good job, {username}! You guessed my number in {guesses_taken} {'try' if guesses_taken == 1 else 'tries'}"
-    )
 else:
-    print(f"Nope! The number I was thinking of was {secret_number}")
+    print(
+        f"You have run out of guesses! The number I was thinking of was {secret_number}"
+    )
